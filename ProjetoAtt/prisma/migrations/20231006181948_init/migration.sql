7eb3e92b4b3ac379fe65351d50ea1e1f967be899
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-11-29 14:43:42 -0300

    Delete ProjetoAtt directory

--- ProjetoAtt/prisma/migrations/20231006181948_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Grupo_estandeId_key";
--- ProjetoAtt/src/models/Avaliador.ts
+++
@@ -1,5 +0,0 @@
-import Avaliacao from "./Avaliacao";
-
-export default interface Avaliador{
-    avaliarGrupo(nomeGrupo: Avaliacao["nomeGrupo"], nota: number): number    
-}
